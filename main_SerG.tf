447216e27d5d1d2b062e3513fc3dffc374a3d651
vladgordey <vladgordey@gmail.com>
2017-02-13 22:00:47 -0500

    Update main_SerG.tf

--- main_SerG.tf
+++ main_SerG.tf
@@ -3,9 +3,9 @@
 # Our default security group to access
 # the instances over SSH and HTTP
 resource "aws_security_group" "default" {
-  name        = "SG for instance"
-  description = "Used in the terraform"
-  vpc_id = "${aws_vpc.main.id}"
+  name        = "${var.short_name}-sg_default"
+  description = "Default SD for terraform"
+  vpc_id = "${var.vpc_id}"
 
   # SSH access from anywhere
   ingress {
@@ -36,8 +36,8 @@ resource "aws_security_group" "default" {
 # the ELB over HTTP
 resource "aws_security_group" "elb" {
   vpc_id = "${aws_vpc.main.id}"
-  name   = "SG for LB"
-  description = "Used in the terraform"
+  name        = "${var.short_name}-sg_elb"
+  description = "ELB SD for terraform"
 
   # HTTP access from anywhere
   ingress {
@@ -61,6 +61,31 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# Our elb security group to access
+# the ELB over HTTP
+resource "aws_security_group" "RDS" {
+  vpc_id = "${aws_vpc.main.id}"
+  name        = "${var.short_name}-RDS"
+  description = "ELB SD for terraform"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [${"aws_security_group.default.id"}]
+  }
+  
   # outbound internet access
   egress {
     from_port   = 0
