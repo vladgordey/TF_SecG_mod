5fd518dfae2d3be39a039ed04695bc96f42479c2
vladgordey <vladgordey@gmail.com>
2017-02-14 19:48:03 -0500

    Update main_SerG.tf

--- main_SerG.tf
+++ main_SerG.tf
@@ -9,24 +9,24 @@ resource "aws_security_group" "default" {
 
   # SSH access from anywhere
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = "22"
+    to_port     = "22"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # HTTP access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = "80"
+    to_port     = "80"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = "0"
+    to_port     = "0"
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -41,30 +41,30 @@ resource "aws_security_group" "elb" {
 
   # HTTP access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = "80"
+    to_port     = "80"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   # HTTPS access from anywhere
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = "443"
+    to_port     = "443"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   # SSH access from anywhere
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = "22"
+    to_port     = "22"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = "0"
+    to_port     = "0"
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -80,16 +80,16 @@ resource "aws_security_group" "RDS" {
 
   # HTTP access from anywhere
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = "3306"
+    to_port     = "3306"
     protocol    = "tcp"
     security_groups = [${"aws_security_group.default.id"}]
   }
   
   # outbound internet access
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = "0"
+    to_port     = "0"
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
